fe7763c412d52990a79c4efe58708191030a7912
janekandrz <146929131+janekandrz@users.noreply.github.com>
2024-10-11 13:24:50 +0200

    laby 0

--- laby automatyka/lab0.m
+++ laby automatyka/lab0.m
@@ -1,19 +1,29 @@
-t = 0;0.05;20;
+close all
 
-y1= sin(t);
+t = 0:0.05:20;
+
+y1 = sin(t);
 
 y2 = sin(t*2) + sin(t*3);
 
 model = tf(5, [6 2 1]);
 
-figure; 
+P = 1 ;
+I = 1 ;
+D = 4 ;
+
+figure;
 step(model)
 
-figure; 
+figure;
 impulse(model)
 
 figure;
-lsim(model, y1', t');
+lsim(model, y1, t);
 
+
+input.time = t';
+input.signals.values = y1';
+input.signals.dimensions = 1;
 
 
